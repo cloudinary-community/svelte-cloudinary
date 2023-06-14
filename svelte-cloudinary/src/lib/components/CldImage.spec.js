3c2fdec1a8e6b74b51def7ed21ff21a4a91b723e
Matías Hernández Arellano <matiasfh@gmail.com>
2023-06-14 11:20:12 -0400

    fix: fix issues on beta for CldImage (#15)
    # Description
    
    <!-- Include a summary of the change made and also list the dependencies
    that are required if any -->
    
    ## Issue Ticket Number
    
    Fixes #11 #13 #12 #10 
    
    Also update doc examples
    
    <!-- Specify above which issue this fixes by referencing the issue
    number (`#<ISSUE_NUMBER>`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/cloudinary-community/svelte-cloudinary/issues/<ISSUE_NUMBER>
    -->
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [X] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [X] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [ ] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/cloudinary-community/svelte-cloudinary/issues)
    ticket for this PR
    - [ ] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/cloudinary-community/svelte-cloudinary/pulls)
    for the same update/change?
    - [ ] I have performed a self-review of my own code
    - [ ] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

--- svelte-cloudinary/src/lib/components/CldImage.spec.js
+++ svelte-cloudinary/src/lib/components/CldImage.spec.js
@@ -6,7 +6,7 @@ import CldImage from './CldImage.svelte';
 
 describe('CldImage', () => {
     it('should render a Cloudinary image with the given attributes', () => {
-        const expectedUrl  = 'https://res.cloudinary.com/tutorials/image/upload/c_limit,w_300,h_200,f_auto/b_#123456/f_auto/q_auto/sample?_a=E';
+        const expectedUrl  = 'image/upload/c_limit,w_300/b_#123456/f_auto/q_auto/sample?_a=E';
         const expectedSizes = "(min-width: 300px) 300px, 100vw"
         const props = {
             src: 'sample',
@@ -17,11 +17,12 @@ describe('CldImage', () => {
             priority: true,
             background: '#123456',
             cloudName: 'demo'
-            
+
         };
         render(CldImage, { props });
         const img = screen.getByRole('img')
-        expect(img.src).toBe(expectedUrl)
+        expect(img.src).toContain('https://res.cloudinary.com')
+        expect(img.src).toContain(expectedUrl)
         expect(img.sizes).toBe(expectedSizes)
     });
 });
--- svelte-cloudinary/vite.config.ts
+++ svelte-cloudinary/vite.config.ts
@@ -1,7 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
+	server: {
+		fs: {
+			allow: [searchForWorkspaceRoot(process.cwd()), './package.json']
+		}
+	},
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
