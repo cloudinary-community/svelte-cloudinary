6bf3f659210028140701338cb2392beb141d8712
Matias Hernandez <matiasfh@gmail.com>
2023-06-15 06:29:11 -0400

    fix: patch update, copy README and LICENSE on build

--- svelte-cloudinary/scripts/copy-package.js
+++ svelte-cloudinary/scripts/copy-package.js
@@ -1,13 +1,11 @@
 import fs from 'fs';
+import path from 'path'
+const root = process.cwd()
+const filesToCopy = ['./package.json', './README.md', "./LICENSE"];
+filesToCopy.forEach(file => {
+  const __dirname = path.dirname(root);
+  fs.copyFileSync(path.resolve(__dirname,file), `./dist/${file}`);
+})
 
-// read file into JSON
-const pkg = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+console.log('files written to /dist');
 
-// adjust pkg json however you like ..
-
-// write it to your output directory
-fs.writeFileSync(
-  './dist/package.json', // path to your output directory may vary
-  JSON.stringify(pkg, null, 2)
-);
-console.log('package.json written to /dist')
