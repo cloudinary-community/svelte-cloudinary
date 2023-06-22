18ade13c6f7b4b1963c83a8de4f0cf4562a49faf
Matias Hernandez <matiasfh@gmail.com>
2023-06-22 12:35:30 -0400

    fix: patch update, upgrade to svelte 4, fix analytics

--- svelte-cloudinary/src/lib/constants/analytics.ts
+++ svelte-cloudinary/src/lib/constants/analytics.ts
@@ -1,6 +1,6 @@
 import sveltePkg from 'svelte/package.json';
 import metadata from '../metadata.json'
 export const SVELTE_CLOUDINARY_ANALYTICS_ID = 'E';
-export const SVELTE_CLOUDINARY_VERSION = parseInt(metadata.version);
+export const SVELTE_CLOUDINARY_VERSION = metadata.version.split('-')[0]
 export const SVELTE_VERSION = `${sveltePkg.version.split('.')[0]}.0.0`;
 
