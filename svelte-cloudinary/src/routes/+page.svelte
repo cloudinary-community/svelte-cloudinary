372dbfd29ec1fd3f17741281e5e15062dbe41f04
Matias Hernandez <matiasfh@gmail.com>
2023-07-11 12:20:41 -0400

    fix: patch update allow reactive props for src, alt, width and height

--- svelte-cloudinary/src/routes/+page.svelte
+++ svelte-cloudinary/src/routes/+page.svelte
@@ -1,83 +1,51 @@
 <script lang="ts">
 	import CldImage from '$lib/components/CldImage.svelte';
+	const images = [
+		{ fileName: 'woman-headphones' },
+		{ fileName: 'turtle' },
+		{ fileName: 'sneakers' }
+	];
+	let mainImage = 'earth';
+
+	const handleSelectImage = (imgIndex: number) => {
+		mainImage = images[imgIndex].fileName;
+	};
 </script>
 
-<!-- <CldImage -->
-<!-- 	width="200" -->
-<!-- 	height={200} -->
-<!-- 	src="images/woman-headphones" -->
-<!-- 	alt="Original image of images/woman with headphones" -->
-<!-- 	config={{ -->
-<!-- 		cloud: { -->
-<!-- 			cloudName: 'next-cloudinary' -->
-<!-- 		}, -->
-<!-- 		url: { secureDistribution: 'spacejelly.dev' } -->
-<!-- 	}} -->
-<!-- /> -->
-<CldImage
-	height="300"
-	width={300}
-	src="images/woman-headphones"
-	removeBackground
-	tint="70:blue:green:purple"
-	underlay="images/city-skyline"
-	sizes="100vw"
-	alt="Description of my image"
-/>
-<!-- <CldImage -->
-<!-- 	width={1200} -->
-<!-- 	height={1200} -->
-<!-- 	src="images/woman-headphones" -->
-<!-- 	crop="thumb" -->
-<!-- 	removeBackground -->
-<!-- 	tint="70:blue:green:purple" -->
-<!-- 	underlay="images/city-skyline" -->
-<!-- 	alt="Description of my image" -->
-<!-- /> -->
-<!---->
-<!-- <CldImage -->
-<!-- 	width={987} -->
-<!-- 	height={987} -->
-<!-- 	src="images/woman-headphones" -->
-<!-- 	sizes="50vw" -->
-<!-- 	crop="thumb" -->
-<!-- 	tint="40:253f8c" -->
-<!-- 	gravity="faces" -->
-<!-- 	underlay="images/city-skyline" -->
-<!-- 	overlays={[ -->
-<!-- 		{ -->
-<!-- 			position: { -->
-<!-- 				gravity: 'north', -->
-<!-- 				y: 60 -->
-<!-- 			}, -->
-<!-- 			text: { -->
-<!-- 				color: 'rgb:52a4ff80', -->
-<!-- 				fontFamily: 'Source Sans Pro', -->
-<!-- 				fontSize: 320, -->
-<!-- 				fontWeight: 'black', -->
-<!-- 				text: 'MUSIC', -->
-<!-- 				letterSpacing: -10, -->
-<!-- 				lineSpacing: -100, -->
-<!-- 				stroke: true, -->
-<!-- 				border: '20px_solid_rgb:2d0eff99' -->
-<!-- 			} -->
-<!-- 		}, -->
-<!-- 		{ -->
-<!-- 			position: { -->
-<!-- 				gravity: 'south', -->
-<!-- 				y: 60 -->
-<!-- 			}, -->
-<!-- 			text: { -->
-<!-- 				color: 'rgb:52a4ff80', -->
-<!-- 				fontFamily: 'Source Sans Pro', -->
-<!-- 				fontSize: 320, -->
-<!-- 				fontWeight: 'black', -->
-<!-- 				text: 'IS LIFE', -->
-<!-- 				letterSpacing: -10, -->
-<!-- 				lineSpacing: -100, -->
-<!-- 				stroke: true, -->
-<!-- 				border: '20px_solid_rgb:2d0eff99' -->
-<!-- 			} -->
-<!-- 		} -->
-<!-- 	]} -->
-<!-- /> -->
+<div>
+	<h1>Cloudinary Image</h1>
+	<CldImage
+		layout="fullWidth"
+		height="180"
+		objectFit="contain"
+		src={`images/${mainImage}`}
+		alt={mainImage}
+		quality="80"
+	/>
+</div>
+<div>
+	<h1>Regular Image</h1>
+	<h2>{mainImage}</h2>
+	<img
+		height={180}
+		width="auto"
+		src={`https://res.cloudinary.com/svelte-cloudinary/image/upload/f_auto/q_80/v1/images/${mainImage}`}
+		alt=""
+	/>
+</div>
+<div>
+	<h2>buttons</h2>
+	{#each images as image, i}
+		<button on:click={() => handleSelectImage(i)}>
+			<CldImage
+				class="cursor-pointer"
+				layout="fullWidth"
+				height={150}
+				objectFit="cover"
+				src={`images/${image.fileName}`}
+				alt={image.fileName}
+				quality="80"
+			/>
+		</button>
+	{/each}
+</div>
