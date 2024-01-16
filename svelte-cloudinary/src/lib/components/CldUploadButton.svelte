092e8143eaa2eae5a52119de956b59d50ceea7de
Matías Hernández Arellano <matiasfh@gmail.com>
2024-01-16 15:36:14 -0300

    feat #55 enable upload widget instance methods (#78)
    # Description
    
    <!-- Include a summary of the change made and also list the dependencies
    that are required if any -->
    Updates the CldUploadWidtet component to enable all of the cloudinary
    instance methods.
    
    Also updates de CldUploadWidget documentation
    
    ## Issue Ticket Number
    
    Fixes #55
    Fixes #67 
    Fixes #69 
    Fixes #75 
    Fixes #76 
    
    <!-- Specify above which issue this fixes by referencing the issue
    number (`#<ISSUE_NUMBER>`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/cloudinary-community/svelte-cloudinary/issues/<ISSUE_NUMBER>
    -->
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [x] Fix or improve the documentation
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [ ] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/cloudinary-community/svelte-cloudinary/issues)
    ticket for this PR
    - [ ] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/cloudinary-community/svelte-cloudinary/pulls)
    for the same update/change?
    - [ ] I have performed a self-review of my own code
    - [ ] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

--- svelte-cloudinary/src/lib/components/CldUploadButton.svelte
+++ svelte-cloudinary/src/lib/components/CldUploadButton.svelte
@@ -2,22 +2,45 @@
 	import type { HTMLButtonAttributes } from 'svelte/elements';
 	import CldUploadWidget from './CldUploadWidget.svelte';
 	import type { CldUploadWidgetProps } from './CldUploadWidgetTypes.ts';
-	import { invariant } from '$lib/util.js';
 
 	type $$Props = CldUploadWidgetProps & HTMLButtonAttributes;
 	// destructure the props
 	const {
-		uploadPreset,
-		signatureEndpoint,
+		children,
 		onError,
-		onUpload,
-		options,
 		onOpen,
+		onUpload,
+		onAbort,
+		onBatchCancelled,
 		onClose,
+		onDisplayChanged,
+		onPublicId,
+		onQueuesEnd,
+		onQueuesStart,
+		onRetry,
+		onShowCompleted,
+		onSourceChanged,
+		onSuccess,
+		onTags,
+		onUploadAdded,
+		options,
+		signatureEndpoint,
+		uploadPreset,
 		...buttonProps
 	} = $$props as $$Props;
 
 	let baseProps: CldUploadWidgetProps = {
+		onAbort,
+		onBatchCancelled,
+		onDisplayChanged,
+		onPublicId,
+		onQueuesEnd,
+		onQueuesStart,
+		onRetry,
+		onShowCompleted,
+		onSourceChanged,
+		onSuccess,
+		onUploadAdded,
 		onClose,
 		onOpen,
 		options,
@@ -26,13 +49,11 @@
 		uploadPreset,
 		signatureEndpoint
 	};
-	// @ts-expect-error the $$slots and $$scope attributes inside the buttonsProps object
 	// appears because of the spread operator on line 39
 	// this attributes should not be passed to the button html elements since are not valid attributes
-	delete buttonProps['$$slots']
+	delete buttonProps['$$slots'];
 	// @ts-expect-error
-	delete buttonProps['$$scope']
-
+	delete buttonProps['$$scope'];
 </script>
 
 <CldUploadWidget {...baseProps} let:open let:isLoading>
