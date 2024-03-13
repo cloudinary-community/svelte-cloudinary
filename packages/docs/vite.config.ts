3753223854f2abab49765cf5ae7800ed6434309f
GHOST <ghostdevbusiness@gmail.com>
2024-03-13 03:29:56 +0000

    chore: remove sentry

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,18 +1,6 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'matias-hernandez',
-				project: 'svelte-cloudinary-docs'
-			}
-		}),
-		sveltekit()
-	]
-	// optimizeDeps: {
-	// 	exclude:  ['style-object-to-css-string']
-	// }
+	plugins: [sveltekit()]
 });
