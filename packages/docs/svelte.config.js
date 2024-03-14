a91609c595ccbca9d41626baeff92504da7b101b
GHOST <ghostdevbusiness@gmail.com>
2024-03-14 00:19:37 +0000

    build: use node 18

--- packages/docs/svelte.config.js
+++ packages/docs/svelte.config.js
@@ -23,7 +23,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			runtime: 'nodejs20.x'
+			runtime: 'nodejs18.x'
 		})
 	}
 };
