54c2b34c0813ea0d56782d69a3b3d3996c5b38b0
GHOST <ghostdevbusiness@gmail.com>
2024-03-14 00:30:11 +0000

    chore: update playground deps

--- packages/playground/svelte.config.js
+++ packages/playground/svelte.config.js
@@ -1,6 +1,6 @@
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
