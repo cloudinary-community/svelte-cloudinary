2bd994219edf05d00db26c59dfef985ba3ff1caf
GHOST <ghostdevbusiness@gmail.com>
2024-04-16 03:53:26 +0100

    chore(playground): use new config system

--- packages/playground/src/routes/+layout.svelte
+++ packages/playground/src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import { configureCloudinary } from 'svelte-cloudinary';
 	import '../app.postcss';
+
+	configureCloudinary(import.meta.env.VITE_PUBLIC_CLOUDINARY_CLOUD_NAME);
 </script>
 
 <slot />
