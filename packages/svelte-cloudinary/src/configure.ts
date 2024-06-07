359cffb90e7790beeea3c135fe02cef9bc49c8ba
GHOST <ghostdevbusiness@gmail.com>
2024-06-07 22:48:06 +0100

    feat: support analytics in configureCloudinary

--- packages/svelte-cloudinary/src/configure.ts
+++ packages/svelte-cloudinary/src/configure.ts
@@ -1,25 +1,48 @@
-import { type ConfigOptions } from '@cloudinary-util/url-loader';
-import { writable, type Writable } from 'svelte/store';
+import type { ConfigOptions, AnalyticsOptions } from '@cloudinary-util/url-loader';
+import { get, writable, type Writable } from 'svelte/store';
 import { setContext, getContext } from 'svelte';
+import sveltePkg from 'svelte/package.json';
 
 const STORE_KEY = 'svelte-cloudinary-v2-config';
 
-export function toConfig(configOrName: string | ConfigOptions): ConfigOptions {
-	return typeof configOrName == 'string' ? { cloud: { cloudName: configOrName } } : configOrName;
+interface SvelteCloudinaryConfig extends ConfigOptions {
+	analytics?: AnalyticsOptions;
 }
 
-export function configureCloudinary(configOrName: string | ConfigOptions) {
-	setContext(STORE_KEY, writable(toConfig(configOrName)));
+export type ConfigOrName = string | SvelteCloudinaryConfig;
+
+const DEFAULT_ANALYTICS: AnalyticsOptions = Object.freeze({
+	techVersion: sveltePkg.version,
+	sdkSemver: 'TODO',
+	sdkCode: 'E',
+	product: 'B',
+	feature: ''
+});
+
+export function toConfig(configOrName: ConfigOrName): SvelteCloudinaryConfig {
+	return typeof configOrName == 'string'
+		? { cloud: { cloudName: configOrName }, analytics: DEFAULT_ANALYTICS }
+		: Object.assign({ analytics: DEFAULT_ANALYTICS }, configOrName);
 }
 
-export function getConfigStore(): Writable<ConfigOptions> {
+type ConfigStore = Writable<SvelteCloudinaryConfig>;
+
+export function configureCloudinary(configOrName: ConfigOrName) {
+	setContext<ConfigStore>(STORE_KEY, writable(toConfig(configOrName)));
+}
+
+export function getConfig(): SvelteCloudinaryConfig {
+	return get(getConfigStore());
+}
+
+export function getConfigStore(): ConfigStore {
 	try {
-		const currentStore = getContext<Writable<ConfigOptions>>(STORE_KEY);
+		const currentStore = getContext<ConfigStore>(STORE_KEY);
 		if (currentStore) return currentStore;
 
-		console.warn('[svelte-cloudinary] Config store is empty, did ');
+		console.warn('[svelte-cloudinary] Config store is empty, did you call configureCloudinary?');
 		configureCloudinary({});
-		return getContext<Writable<ConfigOptions>>(STORE_KEY);
+		return getContext<ConfigStore>(STORE_KEY);
 	} catch (error) {
 		throw new Error(
 			'[svelte-cloudinary] Unable to get config store, did you call configureCloudinary?',
