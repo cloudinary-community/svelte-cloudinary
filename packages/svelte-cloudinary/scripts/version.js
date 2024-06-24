43bedfe048ed5964a130d1b81b71379a4d482c84
GHOST <ghostdevbusiness@gmail.com>
2024-06-24 03:08:13 +0100

    chore: update packaging/publishing

--- packages/svelte-cloudinary/scripts/version.js
+++ packages/svelte-cloudinary/scripts/version.js
@@ -1,10 +1,10 @@
 import { readFile, writeFile } from 'node:fs/promises';
 import { join } from 'desm';
 
-const pkg = JSON.parse(await readFile(join('../package.json'), 'utf-8'));
+const pkg = JSON.parse(await readFile(join(import.meta.url, '../package.json'), 'utf-8'));
 
 const tpl = `// THIS FILE IS GENERATED
 export const VERSION = '${pkg.version}';
 `;
 
-await writeFile(join('../src/version.ts'), tpl, 'utf-8');
+await writeFile(join(import.meta.url, '../src/version.ts'), tpl, 'utf-8');
