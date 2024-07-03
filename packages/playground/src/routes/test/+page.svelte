d0491b315c65f85d48b6f0d033c2f6c1dca83e81
GHOST <ghostdevbusiness@gmail.com>
2024-07-03 23:26:56 +0100

    chore: update playground

--- packages/playground/src/routes/test/+page.svelte
+++ packages/playground/src/routes/test/+page.svelte
@@ -6,6 +6,11 @@
 	uploadPreset="svelte-cloudinary-signed"
 	signatureEndpoint="/api/sign-cloudinary-params"
 	let:open
->
-	<button on:click={() => open()}>Open the widget</button>
+	let:isLoading>
+	<button
+		class="disabled:opacity-60 disabled:cursor-not-allowed rounded-md bg-indigo-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+		on:click={() => open()}
+		disabled={isLoading}>
+		Open the widget
+	</button>
 </CldUploadWidget>
