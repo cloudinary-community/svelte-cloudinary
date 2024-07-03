af16632a2401a556d22bb04311b16ffb4a36707b
GHOST <ghostdevbusiness@gmail.com>
2024-07-03 23:26:29 +0100

    feat: update clduploadbutton component

--- packages/svelte-cloudinary/src/components/CldUploadButton.svelte
+++ packages/svelte-cloudinary/src/components/CldUploadButton.svelte
@@ -1,63 +1,41 @@
+<!--
+	@component
+	
+	This component provides a button for rendering the Cloudinary Upload Widget.
+	It's recommended you use the <CldUploadWidget /> component and bring your own button.
+
+	@see https://svelte.cloudinary.dev/components/upload-button
+
+	@example Upload Button
+
+	```svelte
+	<script>
+		import { CldUploadButton } from 'svelte-cloudinary';
+	</script>
+
+	<CldUploadWidget uploadPreset="<your upload preset>" />
+	```
+-->
+
 <script lang="ts">
+	import type { CldUploadWidgetProps } from './CldUploadWidget.svelte';
 	import type { HTMLButtonAttributes } from 'svelte/elements';
 	import CldUploadWidget from './CldUploadWidget.svelte';
-	import type { CldUploadWidgetProps } from './CldUploadWidgetTypes.ts';
 
 	type $$Props = CldUploadWidgetProps & HTMLButtonAttributes;
-	// destructure the props
-	const {
-		children,
-		onError,
-		onOpen,
-		onUpload,
-		onAbort,
-		onBatchCancelled,
-		onClose,
-		onDisplayChanged,
-		onPublicId,
-		onQueuesEnd,
-		onQueuesStart,
-		onRetry,
-		onShowCompleted,
-		onSourceChanged,
-		onSuccess,
-		onTags,
-		onUploadAdded,
-		options,
-		signatureEndpoint,
-		uploadPreset,
-		...buttonProps
-	} = $$props as $$Props;
 
-	let baseProps: CldUploadWidgetProps = {
-		onAbort,
-		onBatchCancelled,
-		onDisplayChanged,
-		onPublicId,
-		onQueuesEnd,
-		onQueuesStart,
-		onRetry,
-		onShowCompleted,
-		onSourceChanged,
-		onSuccess,
-		onUploadAdded,
-		onClose,
-		onOpen,
-		options,
-		onUpload,
-		onError,
-		uploadPreset,
-		signatureEndpoint
-	};
-	// appears because of the spread operator on line 39
-	// this attributes should not be passed to the button html elements since are not valid attributes
-	delete buttonProps['$$slots'];
-	// @ts-expect-error
-	delete buttonProps['$$scope'];
+	$: ({ config, uploadPreset, signatureEndpoint, options, ...buttonProps } =
+		$$props as $$Props);
 </script>
 
-<CldUploadWidget {...baseProps} let:open let:isLoading>
-	<button {...buttonProps} on:click|preventDefault={open} disabled={isLoading}>
+<CldUploadWidget
+	{config}
+	{uploadPreset}
+	{signatureEndpoint}
+	{options}
+	let:open
+	let:isLoading>
+	<button {...buttonProps} on:click={() => open()} disabled={isLoading}>
 		<slot>Upload</slot>
 	</button>
 </CldUploadWidget>
