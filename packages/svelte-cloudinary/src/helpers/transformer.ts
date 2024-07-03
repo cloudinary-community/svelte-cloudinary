841dff0653822300474f2a70f27a58b25e928644
GHOST <ghostdevbusiness@gmail.com>
2024-07-03 22:37:17 +0100

    chore: update transformer

--- packages/svelte-cloudinary/src/helpers/transformer.ts
+++ packages/svelte-cloudinary/src/helpers/transformer.ts
@@ -2,13 +2,12 @@ import type { CldImageProps } from '../components/CldImage.svelte';
 import { getCldImageUrl } from './getCldImageUrl';
 import type { ImageProps } from '@unpic/svelte';
 
-export function createTransformer(props: CldImageProps): ImageProps['transformer'] {
-	return (loaderOptions) => {
-		const options = structuredClone(props);
+type URLTransformer = Exclude<ImageProps['transformer'], undefined>;
 
-		// todo make typescript happy
-		options.width = parseInt(`${options.width}`);
-		options.height = parseInt(`${options.height}`);
+export function createTransformer(props: CldImageProps): URLTransformer {
+	return (loaderOptions) => {
+		let height = parseInt(`${props.height}`);
+		let width = parseInt(`${props.width}`);
 
 		// The loader options are used to create dynamic sizing when working with responsive images
 		// so these should override the default options collected from the props alone if
@@ -18,29 +17,35 @@ export function createTransformer(props: CldImageProps): ImageProps['transformer
 		if (
 			typeof loaderOptions.width == 'number' &&
 			typeof loaderOptions.width == 'number' &&
-			loaderOptions.width !== options.width
+			loaderOptions.width !== width
 		) {
-			const multiplier = loaderOptions.width / options.width;
-
-			options.width = loaderOptions.width;
+			width = loaderOptions.width;
 
 			// The height may change based off of the value passed through via the loader options
 			// In an example where the user sizes is 800x600, but the loader is passing in 400
 			// due to responsive sizing, we want to ensure we're using a height that will
 			// resolve to the same aspect ratio
 
-			if (typeof options.height === 'number') {
-				options.height = Math.floor(options.height * multiplier);
+			if (typeof height === 'number') {
+				const multiplier = loaderOptions.width / width;
+				height = Math.floor(height * multiplier);
 			}
-		} else if (typeof loaderOptions?.width === 'number' && typeof options?.width !== 'number') {
+		} else if (
+			typeof loaderOptions?.width === 'number' &&
+			typeof width !== 'number'
+		) {
 			// If we don't have a width on the options object, this may mean that the component is using
 			// the fill option: https://nextjs.org/docs/pages/api-reference/components/image#fill
 			// The Fill option does not allow someone to pass in a width or a height
 			// If this is the case, we still need to define a width for sizing optimization but also
 			// for responsive sizing to take effect, so we can utilize the loader width for the base width
-			options.width = loaderOptions?.width;
+			width = loaderOptions?.width;
 		}
 
-		return getCldImageUrl(props);
+		return getCldImageUrl({
+			...props,
+			height,
+			width,
+		});
 	};
 }
