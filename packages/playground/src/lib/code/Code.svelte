0abb4ed09677e22466afeb2ba677900a8d4bb5e5
GHOST <ghostdevbusiness@gmail.com>
2024-08-28 03:00:58 +0100

    fix(playground): codeblock overflow

--- packages/playground/src/lib/code/Code.svelte
+++ packages/playground/src/lib/code/Code.svelte
@@ -10,7 +10,8 @@
 	});
 </script>
 
-<div class="relative bg-neutral-900 px-8 py-6 w-[400px] rounded-2xl">
+<div
+	class="relative bg-neutral-900 px-8 py-6 w-[400px] rounded-2xl overflow-x-auto">
 	<div class="absolute right-3 top-3">
 		<CopyButton text={code} />
 	</div>
