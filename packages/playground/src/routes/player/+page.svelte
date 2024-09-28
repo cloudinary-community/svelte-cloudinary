8d321960b0321c68f9e2d6a9efd0189db7f481b7
GHOST <ghostdevbusiness@gmail.com>
2024-09-28 22:51:22 +0100

    wip: video player on playground

---
+++ packages/playground/src/routes/player/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { CldVideoPlayer } from 'svelte-cloudinary';
+</script>
+
+<main class="flex flex-col gap-12 mt-16 items-center">
+	<div class="w-[400px]">
+		<CldVideoPlayer src="videos/mountain-stars" />
+	</div>
+</main>
