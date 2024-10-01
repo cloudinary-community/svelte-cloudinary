f9666a8e9f9f74737118220245f7750fc85586fe
Ovindu Atukorala <ovindu.atukorala@gmail.com>
2024-10-01 03:29:41 +0000

    add jsdoc descriptions to the helpers #144

--- packages/svelte-cloudinary/src/helpers/getCldOgImageUrl.ts
+++ packages/svelte-cloudinary/src/helpers/getCldOgImageUrl.ts
@@ -5,6 +5,27 @@ import type {
 	ImageOptions,
 } from '@cloudinary-util/url-loader';
 
+
+/**
+ * This can be used  to get a Cloudinary Open Graph image URL outside the component.
+ * 
+ * This helper provides a wide range of options for being able to easily generate social cards for Open Graph images.
+ * 
+ * Configuration for getCldOgImageUrl is the same as getCldImageUrl. 
+ * The only difference is getCldOgImageUrl provides the following default settings to make generating an OG image URL simpler.
+ * @see https://svelte.cloudinary.dev/helpers/getcldogimageurl/configuration
+ * 
+ * @example
+ * <script>
+ * 	import { getCldOgImageUrl } from 'svelte-cloudinary';
+ * 
+ * 	const url = getCldOgImageUrl({
+ *  	src: 'samples/sea-turtle',   // The public ID of the image in Cloudinary
+ * 	});
+ * 
+ * 	console.log(url); // Outputs the Cloudinary OG image URL with the specified src
+ * </script>
+ */
 export function getCldOgImageUrl(
 	options: ImageOptions,
 	configOverride?: ConfigOptions,
