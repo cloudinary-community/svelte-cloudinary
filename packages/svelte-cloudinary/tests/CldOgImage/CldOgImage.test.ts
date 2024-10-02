02e7118d5c177a0ca8f1872e8ff9b2e39b61acbe
Ovindu Atukorala <ovindu.atukorala@gmail.com>
2024-10-02 17:49:32 +0000

    Minor changes to tests for CldOgImage

--- packages/svelte-cloudinary/tests/CldOgImage/CldOgImage.test.ts
+++ packages/svelte-cloudinary/tests/CldOgImage/CldOgImage.test.ts
@@ -7,60 +7,56 @@ describe('CldImage', () => {
 		cleanup();
 	});
 
-    it('should generate the correct URL for Cloudinary Open Graph image in the og:image meta tag', () => {
-        render(CldOgImage, {
-          props: {
-            src: 'og_image',
+	it('should generate the correct URL for Cloudinary Open Graph image in the og:image meta tag', () => {
+		render(CldOgImage, {
+			props: {
+				src: 'og_image',
+				alt: 'OG Image',
+				config: { cloud: { cloudName: 'testing-og' } },
+			},
+		});
 
-            config: { cloud: { cloudName: 'testing-og' } },
-          },
-        });
-    
-        const metaTag = document.querySelector('meta[property="og:image"]');
-        
-        if (metaTag instanceof HTMLMetaElement) {
-          expect(metaTag.content).toContain('https://res.cloudinary.com');
-          expect(metaTag.content).toContain('og_image');
-        } else {
-          throw new Error('og:image meta tag not found');
-        }
-      });
-    
-      it('should render with a dynamically generated Cloudinary URL using a random cloud name', () => {
-        const cloudName = crypto.randomUUID();
-    
-        render(CldOgImage, {
-          props: {
-            src: 'og_image',
-            config: { cloud: { cloudName } },
-          },
-        });
-    
-        const metaTag = document.querySelector('meta[property="og:image"]');
-    
-        if (metaTag instanceof HTMLMetaElement) {
-          expect(metaTag.content).toContain(cloudName);
-        } else {
-          throw new Error('og:image meta tag not found');
-        }
-      });
-    
-      it('should work with global config from environment variables', () => {
-        const cloudName = crypto.randomUUID();
-        vi.stubEnv('VITE_CLOUDINARY_CLOUD_NAME', cloudName);
-    
-        render(CldOgImage, {
-          props: {
-            src: 'og_image',
-          },
-        });
-    
-        const metaTag = document.querySelector('meta[property="og:image"]');
-        if (metaTag instanceof HTMLMetaElement) {
-          expect(metaTag.content).toContain(cloudName);
-        } else {
-          throw new Error('og:image meta tag not found');
-        }
-      });
+		const metaTag = document.querySelector<HTMLMetaElement>(
+			'meta[property="og:image"]',
+		);
+
+		expect(metaTag).toBeInstanceOf(HTMLMetaElement);
+		expect(metaTag?.content).toContain('https://res.cloudinary.com');
+		expect(metaTag?.content).toContain('og_image');
+	});
+
+	it('should render with a dynamically generated Cloudinary URL using a random cloud name', () => {
+		const cloudName = crypto.randomUUID();
+
+		render(CldOgImage, {
+			props: {
+				src: 'og_image',
+				config: { cloud: { cloudName } },
+			},
+		});
+
+		const metaTag = document.querySelector<HTMLMetaElement>(
+			'meta[property="og:image"]',
+		);
+
+		expect(metaTag).toBeInstanceOf(HTMLMetaElement);
+		expect(metaTag?.content).toContain(cloudName);
+	});
+
+	it('should work with global config from environment variables', () => {
+		const cloudName = crypto.randomUUID();
+		vi.stubEnv('VITE_CLOUDINARY_CLOUD_NAME', cloudName);
+
+		render(CldOgImage, {
+			props: {
+				src: 'og_image',
+			},
+		});
+
+		const metaTag = document.querySelector<HTMLMetaElement>(
+			'meta[property="og:image"]',
+		);
+
+		expect(metaTag?.content).toContain(cloudName);
+	});
 });
-
